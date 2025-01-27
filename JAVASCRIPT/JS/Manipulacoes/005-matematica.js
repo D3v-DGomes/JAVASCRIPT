3e8654ede78900a6cb219420a40be642222007aa
D3v-DGomes <devgomes1223@hotmail.com>
2025-01-27 20:00:21 -0300

    Add files via upload

---
+++ JAVASCRIPT/JS/Manipulacoes/005-matematica.js
@@ -0,0 +1,35 @@
+let valor = Math.PI;        // Imprime o valor de PI
+console.log(valor);
+
+let valor_1 = Math.round(3.4);      // Arredonda o número por proximidade
+console.log(valor_1);
+
+let valor_2 = Math.floor(2.78);     // Arredonda para menos forçadamente
+console.log(valor_2);
+
+let valor_3 = Math.ceil(4.2);       // Arredonda para mais forçadamente
+console.log(valor_3);   
+
+let valor_4 = Math.abs(-100);       // Para saber o número absoluto escolhido
+console.log(valor_4);
+
+let valor_5 = Math.min(2, 56, 98);      // Descobrir o menor número entre os digitados
+console.log(valor_5);
+
+let valor_6 = Math.max(534, 982, 302);  // Descobrir o maior número entre os digitados
+console.log(valor_6);    
+
+let valor_7 = Math.random();    // Gera um número aleatório entre 0 e 1
+console.log(valor_7);
+
+let valor_7_1 = Math.floor(Math.random() * 100);    // Gerando número aleatório menor que 100
+console.log(valor_7_1);
+
+let valor_8 = Math.cos(1);      // Saber o cosseno de um número
+console.log(valor_8);
+
+let valor_9 = Math.sin(2);      // Saber o seno de um número
+console.log(valor_9);
+
+let valor_10 = Math.tan(1);     // Saber a tangente de um número
+console.log(valor_10);
