68af38620045cdb86173ca7803537379c18e54ca
D3v-DGomes <devgomes1223@hotmail.com>
2025-02-01 14:18:14 -0300

    Update 009-desconstruindo_arrays.js

--- JAVASCRIPT/JS/Manipulacoes/009-desconstruindo_arrays.js
+++ JAVASCRIPT/JS/Manipulacoes/009-desconstruindo_arrays.js
@@ -1,5 +1,30 @@
 let info = ['David Gomes', 'David', 'Gomes', '@dgomess_'];
 
-let [fullName, firstName, lastName, instagram] = info;
 
+// Desconstruindo todos os itens:
+
+let [fullName, firstName, lastName, instagram] = info;
 console.log(fullName, firstName, lastName, instagram);
+
+
+// Desconstruindo itens específicos:
+
+let [fullName_1, , , instagram_1] = info;
+console.log(fullName_1, instagram_1);
+
+
+// Criando um Array e desconstruindo em seguida:
+
+let list = [color, numbers, nickName] = ['Azul', 23, 'Angelo'] 
+console.log(color, numbers, nickName);
+
+
+// Desconstruindo um array usando função:
+
+function toCreate() {
+    return  [1, 2, 3];
+}
+
+let [a, b, c] = toCreate();
+
+console.log(a, b, c);
