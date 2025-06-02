6e0e969e5aa1fd0bafa620aebb85d79f2b7b6b1f
D3v-DGomes <devgomes1223@hotmail.com>
2025-06-02 16:48:54 -0300

    Recomeçando o projeto. Reestruturando o HTML e criando novas constantes

--- JAVASCRIPT/Projetos/assets_projetos/formulario.js
+++ JAVASCRIPT/Projetos/assets_projetos/formulario.js
@@ -1,56 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Formatação de CPF/CNPJ e campos Nome/Fantasia dinâmicos:
     const cpfCnpjInput = document.getElementById('cpfCnpj');
     const cpfCnpjLabel = document.querySelector('#cpfCnpjGroup label');
-    const nomeGroup = document.getElementById('nomeGroup');
+    const nomeLabel = document.getElementById('nomeLabel');
+    const nomeInput = document.getElementById('nomeInput');
     const razaoSocialGroup = document.getElementById('razaoSocialGroup');
-    const fantasiaGroup = document.getElementById('fantasiaGroup');
+    const emailInput = document.getElementById('email');
+    
 
-    cpfCnpjInput.addEventListener('input', () => {
-        let value = cpfCnpjInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    // Campos obrigatórios:
+    const cadastroForm = document.getElementById('cadastroForm');
+    const cpfCnpjHelp = document.getElementById('cpfCnpjHelp');
+    const nomeHelp = document.getElementById('nomeHelp');
+    const razaoSocialHelp = document.getElementById('razaoSocialHelp');
+    const razaoSocialInput = document.getElementById('razaoSocialInput');
+    const emailHelp = document.getElementById('emailHelp');
 
-        if (value.length > 11) {
-            // Formato CNPJ
-            cpfCnpjLabel.textContent = 'CNPJ';
-            nomeGroup.style.display = 'none';
-            razaoSocialGroup.style.display = 'block';
-            fantasiaGroup.style.display = 'block';
-        } else {
-            // Formato CPF
-            cpfCnpjLabel.textContent = 'CPF';
-            nomeGroup.style.display = 'block';
-            razaoSocialGroup.style.display = 'none';
-            fantasiaGroup.style.display = 'none';
-        }
-        cpfCnpjInput.value = value;
-    });
+
 });
-
-// Adicionando validação de campo:
-const form = document.getElementById('cadastroForm');
-
-form.addEventListener('submit', function(e) {
-    // Evitando que o formulário não recarregue a página:
-    e.preventDefault();
-
-    const errorIcon = '<i class="fa-solid fa-circle-exclamation"></i>';
-
-    const name = document.getElementById('nome');
-    const nameValue = name.value;
-    const inputBox = name.closest('#nomeGroup');
-
-    const errorSpan = inputBox.querySelector('.error');
-    errorSpan.innerHTML = '';
-
-    inputBox.classList.remove('invalid');
-
-    if (isEmpty(name)) {
-        errorSpan.innerHTML = `${errorIcon} Este campo é obrigatório`;
-        inputBox.classList.add('invalid');
-        return;
-    }
-});
-
-// Verificando se o campo está vazio:
-function isEmpty(value) {
-    return value === '';
-}
