95eea1ff04f4655eabbca7ea8c80141cce82fe04
D3v-DGomes <devgomes1223@hotmail.com>
2025-06-02 16:52:55 -0300

    Adicionando máscara para o campo de CPF/CNPJ

--- JAVASCRIPT/Projetos/assets_projetos/formulario.js
+++ JAVASCRIPT/Projetos/assets_projetos/formulario.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const nomeInput = document.getElementById('nomeInput');
     const razaoSocialGroup = document.getElementById('razaoSocialGroup');
     const emailInput = document.getElementById('email');
-    
+
 
     // Campos obrigatórios:
     const cadastroForm = document.getElementById('cadastroForm');
@@ -15,6 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const razaoSocialHelp = document.getElementById('razaoSocialHelp');
     const razaoSocialInput = document.getElementById('razaoSocialInput');
     const emailHelp = document.getElementById('emailHelp');
+
+
+
+    // Máscara do CPF/CNPJ:
+    function mascaraCnpjCpf(valor) { // Agora recebe o valor como argumento
+        let cnpjCpf = valor.replace(/\D/g, '');
+
+        if (cnpjCpf.length > 14) {
+            cnpjCpf = cnpjCpf.substring(0, 14);
+        }
+
+        if (cnpjCpf.length <= 11) {
+            cnpjCpf = cnpjCpf.replace(/(\d{3})(\d)/, '$1.$2');
+            cnpjCpf = cnpjCpf.replace(/(\d{3})(\d)/, '$1.$2');
+            cnpjCpf = cnpjCpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        } else {
+            cnpjCpf = cnpjCpf.replace(/^(\d{2})(\d)/, '$1.$2');
+            cnpjCpf = cnpjCpf.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+            cnpjCpf = cnpjCpf.replace(/\.(\d{3})(\d)/, '.$1/$2');
+            cnpjCpf = cnpjCpf.replace(/(\d{4})(\d)/, '$1-$2');
+        }
+        return cnpjCpf;
+    }
+
+
+
+
+
+
 
 
 });
